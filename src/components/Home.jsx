c2d44685c2ee33424fd9fe8a1512563c5ec36c19
gashman0 <gashman878@gmail.com>
2026-03-30 00:34:16 +0100

    Portfolio little fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         justify-center
         lg:justify-between
         px-6
-        lg:px-12
+        
         pt-24
         lg:pt-[18vh]
         overflow-hidden
@@ -22,11 +22,11 @@ const Home = () => {
     >
       {/* Text Section */}
       <div className="text-center lg:text-left max-w-xl">
-        <p className="text-3xl sm:text-4xl md:text-5xl lg:text-[60px] font-bold leading-tight">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-[60px] font-bold leading-tight">
           I'm Gashman.
-        </p>
+        </h1>
         <p className="mt-2 sm:mt-3 text-base sm:text-lg md:text-xl text-gray-200">
-          Frontend Web Developer
+          Fullstack developer
         </p>
       </div>
 
