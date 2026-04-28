86ef5a2c6781df8b83f0a5099c88c3a985f894d7
Victor Tochukwu <gashman878@gmail.com>
2026-04-28 23:55:32 +0100

    Revert "Portfolio little fix"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         justify-center
         lg:justify-between
         px-6
-        
+        lg:px-12
         pt-24
         lg:pt-[18vh]
         overflow-hidden
@@ -22,11 +22,11 @@ const Home = () => {
     >
       {/* Text Section */}
       <div className="text-center lg:text-left max-w-xl">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-[60px] font-bold leading-tight">
+        <p className="text-3xl sm:text-4xl md:text-5xl lg:text-[60px] font-bold leading-tight">
           I'm Gashman.
-        </h1>
+        </p>
         <p className="mt-2 sm:mt-3 text-base sm:text-lg md:text-xl text-gray-200">
-          Fullstack developer
+          Frontend Web Developer
         </p>
       </div>
 
